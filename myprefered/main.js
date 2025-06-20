f415dec58073c9e987cbee0be6d1ec8a6b245890
daba clovis <dabaclovis@yahoo.com>
2025-06-20 11:34:08 -0400

    first

---
+++ myprefered/main.js
@@ -0,0 +1,22 @@
+
+
+const toggleBtn = document.getElementById("toggle-btn");
+const sidebar = document.getElementById("sidebar");
+
+function toggleSidebar() {
+    sidebar.classList.toggle("closed");
+    toggleBtn.classList.toggle("rotate");
+    Array.from(sidebar.getElementsByClassName("show")).forEach(ul => {
+        ul.classList.remove("show");
+        ul.previousElementSibling.classList.remove("rotate");
+    });
+}
+
+function toggleSubMenu(button) {
+    button.nextElementSibling.classList.toggle("show");
+    button.classList.toggle("rotate");
+    if(sidebar.classList.contains("closed")) {
+        sidebar.classList.remove("closed");
+        toggleBtn.classList.remove("rotate");
+    }
+}
