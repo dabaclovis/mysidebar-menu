fdba4cf8c9c7bcc410710322428ec982fcc390ce
dabaclovis <dabaclovis@yahoo.com>
2025-06-22 01:18:13 -0400

    clovis

---
+++ morecode/main.js
@@ -0,0 +1,8 @@
+
+
+
+
+document.querySelector('.sidebar-toggler').addEventListener('click', function() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('collapsed');
+});
